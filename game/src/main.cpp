4aa361473f4e666c3658954dd793de64cdf36fd9
drkong <72712916+HSPLMEPIPMSUTDLHW@users.noreply.github.com>
2023-06-18 18:10:58 -0400

    Finished

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -7,6 +7,20 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 
+bool CheckCollisionLineCircle(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius)
+{
+    Vector2 nearest = NearestPoint(lineStart, lineEnd, circlePosition);
+    return Distance(nearest, circlePosition) <= circleRadius;
+}
+
+Vector2 centripetalAccel(Vector2 v, float ac, bool clockwise)
+{
+    float angle;
+    if (clockwise) angle = 90 * DEG2RAD;
+    else angle = -90 * DEG2RAD;
+    return Rotate(Normalize(v), angle) * ac;
+
+}
 //Class for the Rigidbody
 
 class Rigidbody
@@ -21,6 +35,12 @@ private:
     float width, height;
     float maxSpeed;
     Rectangle dst;
+    Vector2 whiskers[4];
+    Color whiskerColor[4];
+    Vector2 whiskerPoint[4];
+    bool whiskerTouched[4];
+    float ForwardAngle;
+    float avoidencePower;
     std::string name; // Name is like a tag so I can identify objects if they are in a container
 public:
     Rigidbody(float x, float y, float w, float h, Color c, std::string n)
@@ -34,12 +54,33 @@ public:
         name = n;
         angularVel = Normalize(Vector2{ 15, 0 });
         linearVel;
+        for (int i = 0; i < 4; i++)
+        {
+            whiskerColor[i] = GREEN;
+            whiskerTouched[i] = false;
+
+        }
+       
+        
+        avoidencePower = 2;
     }
 
+
+    
     void Update()
     {
         //When update is called,
        const float dt = GetFrameTime();
+       ForwardAngle = atan2f(getVel().y, getVel().x);
+    //   Vector2 Forward = Normalize(Rotate(Vector2{ 175,0 }, ForwardAngle));
+       whiskers[0] = Normalize(Rotate(Vector2{ 175,0 }, ForwardAngle + 25 * DEG2RAD));
+       whiskers[1] = Normalize(Rotate(Vector2{ 175,0 }, ForwardAngle - 25 * DEG2RAD));
+       whiskers[2] = Normalize(Rotate(Vector2{ 175,0 }, ForwardAngle + 80 * DEG2RAD));
+       whiskers[3] = Normalize(Rotate(Vector2{ 175,0 }, ForwardAngle - 80 * DEG2RAD));
+       for (int i = 0; i < 4; i++)
+       {
+           whiskerTouched[i] = false;
+       }
        float currentspeed = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y));
         if (currentspeed > maxSpeed)
         {
@@ -47,6 +88,49 @@ public:
         }
         position = position + (velocity * dt) + (accel * 0.5f * dt * dt);
         velocity = velocity + accel * dt;
+    }
+
+    void CollisionAvoidence(Vector2 obs,float w, float ac)
+    {
+        Vector2 relative = { 1, 0 };
+
+        for (int i = 0; i < 4; i++)
+        {
+
+            Vector2 point = NearestPoint(getPos(),{ (getPos().x + whiskers[i].x * 150) ,(getPos().y + whiskers[i].y * 150) },obs);
+            Vector2 relativeWhisker = Normalize(Rotate(whiskers[i], -ForwardAngle));
+            whiskerColor[i] = GREEN;
+            if (!whiskerTouched[i])
+            {
+
+        
+            if (CheckCollisionLineCircle(getPos(), point, obs, 25))
+            {
+                whiskerColor[i] = RED;
+                whiskerTouched[i] = true;
+                if (atan2f(relativeWhisker.y, relativeWhisker.x) > 0)
+                {
+                    std::cout << "WHISKER False " << (atan2f(relativeWhisker.y, relativeWhisker.x)) * RAD2DEG << "|| " << ForwardAngle * RAD2DEG << "|| " << (atan2f(relativeWhisker.y, relativeWhisker.x) - sqrt(ForwardAngle * ForwardAngle)) * RAD2DEG << std::endl;
+                    setAccel(getAccel() + (centripetalAccel(getVel(), ac, false) * avoidencePower));
+                }
+                else
+                {
+
+                    std::cout << "WHISKER True  " << (atan2f(relativeWhisker.y, relativeWhisker.x)) * RAD2DEG << "|| " << ForwardAngle * RAD2DEG << "|| " << (atan2f(relativeWhisker.y, relativeWhisker.x) - sqrt(ForwardAngle * ForwardAngle)) * RAD2DEG << std::endl;
+                    setAccel(getAccel() + (centripetalAccel(getVel(), ac, true) * avoidencePower));
+                }
+            }
+
+            }
+            else
+            {
+                whiskerColor[i] = RED;
+            }
+            //    std::cout << "WHISKER " << atan2f(whiskers[i].y, whiskers[i].x) << std::endl;
+      
+        }
+        
+
     }
 
     void setAccel(Vector2 a)
@@ -103,7 +187,14 @@ public:
     {
         return  angularVel;
     }
-
+    Vector2 getWhiskerPos(int w)
+    {
+        return whiskers[w];
+    }
+    Color  getWhiskerColor(int w)
+    {
+        return whiskerColor[w];
+    }
      
 };
 
@@ -127,20 +218,7 @@ Vector2 Flee(const Vector2& agentPos, const Vector2& agentVel, const Vector2& ta
     return Seek(agentPos, agentVel, targetPos, desiredSpeed, -accel);
 }
 
-bool CheckCollisionLineCircle(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius)
-{
-    Vector2 nearest = NearestPoint(lineStart, lineEnd, circlePosition);
-    return Distance(nearest, circlePosition) <= circleRadius;
-}
 
-Vector2 centripetalAccel(Vector2 v, float ac, bool clockwise)
-{
-    float angle;
-    if (clockwise) angle = 90 * DEG2RAD;
-    else angle = -90 * DEG2RAD;
-    return Rotate(Normalize(v), angle)*ac;
-
-}
 
 Vector2 Normalized(Vector2 vec)
 {
@@ -164,16 +242,15 @@ int main(void)
     Vector2 pOffset = { 25,25 };
     Vector2 point;
     Vector2 TurnAngle ={ 175,0 };
-   Vector2 Forward;
+    Vector2 Forward = { 0,0 };
    Color rightWhiskerColor = RED;
    Color leftWhiskerColor = RED;
-    std::vector<Rigidbody*> Birds;
-  //  Birds.push_back(new Rigidbody(((SCREEN_WIDTH / 2) - 25), ((SCREEN_HEIGHT / 2) - 25), 50, 50, BLUE,"Blue"));
-  //  Birds.push_back(new Rigidbody(((SCREEN_WIDTH / 3) - 25), ((SCREEN_HEIGHT / 3) - 25), 50, 50, ORANGE, "Orange"));
+    std::vector<Rigidbody*> Obstacles;
+ 
 
     Rigidbody Player(((SCREEN_WIDTH / 2) - 25), ((SCREEN_HEIGHT / 2) - 25), 50, 50, BLUE, "Blue");
     Rigidbody Obstacle1(((SCREEN_WIDTH / 8) - 25), ((SCREEN_HEIGHT / 4) - 25), 50, 50, BLACK,"");
-  //  Rigidbody Obstacle2(((SCREEN_WIDTH / 4) - 25), ((SCREEN_HEIGHT / 1.3) - 25), 50, 50, BLACK,"");
+   
 
     Rectangle playerRec{ Obstacle1.getPos().x, Obstacle1.getPos().y, 50, 50 };
     static Vector2 vel = { 0,0 };
@@ -259,16 +336,15 @@ int main(void)
  
 
         }
-        /*
-        for (const auto Rigidbody : Birds)
+
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
         {
-            Rigidbody->Update();
+            Obstacles.push_back(new Rigidbody(GetMousePosition().x, GetMousePosition().y, 50, 50, BLACK, ""));
         }
-        */
     
 
         if (IsKeyDown(KEY_G))
-        {
+            {
             std::cout << "dt " << dt << std::endl;
         }
 
@@ -276,75 +352,18 @@ int main(void)
             circleColor = PINK;
         else
             circleColor = RED;
-        /*
-                Vector2 playerPosOffsetNormal = Normalize(Vector2{ Player.getVel().x + 25, Player.getVel().y + 25 });
-        Vector2 playerPosOffset = { Player.getVel().x + 25, Player.getVel().y + 25 };
-        Vector2 defaultAngle = { 150 + Player.getPos().x + 25 ,Player.getPos().y + 25 };
-        float ForwardAngle = atan2f(playerPosOffsetNormal.x, playerPosOffsetNormal.y);
-        ForwardAngle = fmodf(ForwardAngle + 2 * PI, 2 * PI);
-
-        Vector2 Forward = Vector2{ cosf(ForwardAngle), sinf(ForwardAngle) };
-        DrawRectangle(Player.getPos().x, Player.getPos().y, 50, 50, Player.getColor());
-        DrawLineV({ Player.getPos().x + 25, Player.getPos().y + 25 }, playerPosOffset+Forward*100, RED);
-        */
-    
+ 
         Vector2 playerPosOffset = { Player.getPos().x + 25,Player.getPos().y + 25 };
         Vector2 defaultAngle = { 150 + Player.getPos().x   ,Player.getPos().y    };
         Vector2 defaultNormal = Vector2{ 175,0 };
-      //  TurnAngle
-        float ForwardAngle = atan2f(Player.getVel().y , Player.getVel().x );
-        Vector2 Forward = Normalize(Rotate(defaultNormal, ForwardAngle));
-        Vector2 WhiskerRight = Normalize(Rotate(Vector2{ 175,0 }, ForwardAngle + 25*DEG2RAD));
-        Vector2 WhiskerLeft = Normalize(Rotate(Vector2{ 175,0 }, ForwardAngle - 25 * DEG2RAD));
+ 
+ 
 
-
-        point = NearestPoint(Player.getPos(), { (Player.getPos().x + Forward.x * 150) ,(Player.getPos().y + Forward.y * 150) }, Obstacle1.getPos());
-        Vector2 pointRight = NearestPoint(Player.getPos(), { (Player.getPos().x + WhiskerRight.x * 150) ,(Player.getPos().y + WhiskerRight.y * 150) }, Obstacle1.getPos());
-        Vector2 pointLeft = NearestPoint(Player.getPos(), { (Player.getPos().x + WhiskerLeft.x * 150) ,(Player.getPos().y + WhiskerLeft.y * 150) }, Obstacle1.getPos());
-        Vector2 RoateFromRight((Rotate(Player.getVel(), 1 * DEG2RAD)));
-        Vector2 RoateFromLeft((Rotate(Player.getVel(), -1 * DEG2RAD)));
-
-       
-    //    DrawRectangle(Player.getPos().x, Player.getPos().y, 50, 50, Player.getColor());
-   
-        if (IsKeyPressed(KEY_H))
+        for (auto Rigidbody : Obstacles)
         {
+            Player.CollisionAvoidence(Rigidbody->getPos(), 25, ac);
+        }
 
-            std::cout << "V " << Forward.x << "||" << Forward.y << std::endl;
-            //     std::cout << "Ob " << Obstacle1.getPos().x << "||" << Obstacle1.getPos().y << std::endl;
-        //    std::cout << "V RIGHT" << RoateFromRight.x << "||" << RoateFromRight.y << std::endl;
-        //    std::cout << "V LEFT" << RoateFromLeft.x << "||" << RoateFromLeft.y << std::endl;
-
-        }
-        if (IsKeyPressed(KEY_H))
-        {
-           
-            std::cout << "V " << Player.getVel().x << "||" << Player.getVel().y << std::endl;
-       //     std::cout << "Ob " << Obstacle1.getPos().x << "||" << Obstacle1.getPos().y << std::endl;
-
-  
-        }
-        if (CheckCollisionLineCircle(Player.getPos(), point, Obstacle1.getPos(),25))
-        {
-         //   std::cout << "P  " << point.x << "||" << point.y << std::endl;
-        }
-        rightWhiskerColor = RED;
-        leftWhiskerColor = RED;
-        if (CheckCollisionLineCircle(Player.getPos(), pointLeft, Obstacle1.getPos(), 25))
-        {
-         //   Player.setVel(Vector2(Rotate(Player.getVel(), 15 * DEG2RAD)));
-          //   TurnAngle = Rotate(TurnAngle, 5 * DEG2RAD);
-            Player.setAccel(Player.getAccel()+(centripetalAccel(Player.getVel(), ac, true)));
-            leftWhiskerColor = GREEN;
-           
-        }
-        if (CheckCollisionLineCircle(Player.getPos(), pointRight, Obstacle1.getPos(), 25))
-        {
-         //  Player.setVel(Vector2(Rotate(Player.getVel(), -15 * DEG2RAD)));
-           //TurnAngle = Rotate(TurnAngle, -5 * DEG2RAD);
-            Player.setAccel(Player.getAccel() + (centripetalAccel(Player.getVel(), ac, false)));
-            rightWhiskerColor = GREEN;
-        }
         Player.Update();
       
         /*
@@ -355,39 +374,26 @@ int main(void)
         */
         DrawCircle(Player.getPos().x, Player.getPos().y, 25, Player.getColor());
         DrawCircle(Obstacle1.getPos().x, Obstacle1.getPos().y, 25, Obstacle1.getColor());
-
+        for (auto obst : Obstacles)
+        {
+            DrawCircle(obst->getPos().x, obst->getPos().y, 25, obst->getColor());
+        }
 
         DrawLineV({ 100,600 }, { 200,600 }, GREEN);
         DrawLineV({ 100,600 }, { (200 + Forward.x * 100),(600 + Forward.y * 100) }, RED);
         DrawLineV({ Player.getPos().x , Player.getPos().y }, defaultAngle, ORANGE);
-        // DrawLineV({ 0,0 }, GetMousePosition(), BLUE);
-        // DrawLineV({ 0,0 }, { (Forward.x * 100),(Forward.y * 100) }, RED);
+ 
+
         DrawLineV({ Player.getPos().x , Player.getPos().y }, { (Player.getPos().x + Forward.x * 150) ,(Player.getPos().y + Forward.y * 150) }, RED);
-        DrawLineV({ Player.getPos().x , Player.getPos().y }, { (Player.getPos().x + WhiskerLeft.x * 150) ,(Player.getPos().y + WhiskerLeft.y * 150) }, leftWhiskerColor);
-        DrawLineV({ Player.getPos().x , Player.getPos().y }, { (Player.getPos().x + WhiskerRight.x * 150) ,(Player.getPos().y + WhiskerRight.y * 150) }, rightWhiskerColor);
+
+        for (int i = 0; i < 4; i++)
+        {
+            DrawLineV({ Player.getPos().x , Player.getPos().y }, (Player.getPos() + Player.getWhiskerPos(i) * 150), Player.getWhiskerColor(i));
+        }
+     
         DrawLineV({ Player.getPos().x , Player.getPos().y }, GetMousePosition(), BLUE);
         DrawLineV({ Player.getPos().x , Player.getPos().y }, { Player.getVel().x + Player.getPos().x  ,Player.getVel().y + Player.getPos().y }, GREEN);
-
-        DrawCircle(point.x, point.y, 5,PINK);
-        DrawCircle(pointRight.x, pointRight.y, 5, PINK);
-        DrawCircle(pointLeft.x, pointLeft.y, 5, PINK);
-       // DrawLineV({ Player.getPos().x + 25, Player.getPos().y + 25 }, { Player.getAccel().x + Player.getPos().x + 25 ,Player.getAccel().y + Player.getPos().y + 25 }, GREEN);
-        /*
-        for (const auto Rigidbody : Birds)
-        {
-            if (CheckCollisionCircleRec(GetMousePosition(), 20, { Rigidbody->getPos().x,  Rigidbody->getPos().y, Rigidbody->getWidth(), Rigidbody->getHeight() }))
-                circleColor = PINK;
-            else
-                circleColor = RED;
-            DrawRectangle(Rigidbody->getPos().x, Rigidbody->getPos().y, 50, 50, Rigidbody->getColor());
-            DrawLineV({ Rigidbody->getPos() .x + 25, Rigidbody->getPos().y + 25 }, { Rigidbody->getVel().x + Rigidbody->getPos().x + 25 ,Rigidbody->getVel().y + Rigidbody->getPos().y + 25 }, RED);
-            DrawLineV({ Rigidbody->getPos().x + 25, Rigidbody->getPos().y + 25 }, { Rigidbody->getAccel().x + Rigidbody->getPos().x + 25 ,Rigidbody->getAccel().y + Rigidbody->getPos().y + 25 }, GREEN);
-        }
-        */
-        //DrawRectangle(Obstacle1.getPos().x, Obstacle1.getPos().y, 50, 50, Obstacle1.getColor());
-       // DrawRectangle(Obstacle2.getPos().x, Obstacle2.getPos().y, 50, 50, Obstacle2.getColor());
-
-      //  std::cout << "Angle " << ForwardAngle << std::endl;
+       
         EndDrawing();
         
     }
