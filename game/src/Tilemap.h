c0f2fd65810fb3500fa482cea8e199223cd35445
drkong <72712916+HSPLMEPIPMSUTDLHW@users.noreply.github.com>
2023-06-30 13:12:49 -0400

    Forgot to make any commits. This is the finished lab

--- game/src/Tilemap.h
+++ game/src/Tilemap.h
@@ -2,19 +2,30 @@
 #include "TileCoord.h"
 #include "raylib.h"
 #include "rlImGui.h"
-const unsigned int MAP_WIDTH = 24;
-const unsigned int MAP_HEIGHT = 32;
+#include <iostream>
+const unsigned int MAP_WIDTH = 28;
+const unsigned int MAP_HEIGHT = 20;
 
+/*
+const TileCoord NORTH = { -1,0 };
+const TileCoord SOUTH = { 1,0 };
+const TileCoord EAST = { 0,1 };
+const TileCoord WEST = { 0,-1};
+*/
+const TileCoord WEST = { -1,0 };
+const TileCoord EAST = { 1,0 };
+const TileCoord SOUTH = { 0,1 };
+const TileCoord NORTH = { 0,-1};
 
 
 enum Tile
 {
 	Floor = 0,
-	Grass,
+	Mountain,
 	Water,
 	Wall,
 	Rock,
-	Mountain,
+	Grass,
 	Count
 		
 };
@@ -24,54 +35,128 @@ class Tilemap
 public:
 	float tileSizeX = 32;
 	float tileSizeY = 32;
+	Color tileColors[ (int)Tile::Count ];
 
 	Tile tiles[MAP_WIDTH][MAP_HEIGHT];
-	Color tileColors{ Tile::Count };
+
 	Tilemap()
 	{
-		tileColors[ Floor ] = GRAY;
+		tileColors[ Floor ] = BEIGE;
 		tileColors[ Grass ] = GREEN;
 		tileColors[ Water ] = BLUE;
 		tileColors[ Wall ] = DARKGRAY;
+		tileColors[Rock] = GRAY;
 		tileColors[ Mountain ]  = LIGHTGRAY;
-
-
-
 	}
 
 	bool isTileTraversable(TileCoord tilePosition)
 	{
-		Tile type = tile[tilePosition.x][tilePosition.y];
+		Tile type = tiles[tilePosition.x][tilePosition.y];
+		if (type == Tile::Floor && !tileOOB(tilePosition)) return true;
+		return false;
 		 
 	}
 
-	Vector2 ScreenPosOfTile(TileCoord tilePosition)
+	Vector2 GetScreenPosOfTile(TileCoord tilePosition)
 	{
 		return{ (float)tilePosition.x * tileSizeX, (float)tilePosition.y * tileSizeY };
+
 	}
-	/*
+
 	void RandomizeTiles()
 	{
 		for (int x = 0; x < MAP_WIDTH; x++)
-		{
+		{ 
 			for (int y = 0; y < MAP_HEIGHT; y++)
 			{
-				float roll = Random(0.0f, 1.0f);
+				tiles[x][y] = (Tile)(rand() % (int)(Tile::Water));
 			}
 		}
 	}
-	*/
 
-	void DrawTiles()
+	std::vector<TileCoord> GetAdjacentTiles(TileCoord tileposition)
+	{
+		std::vector<TileCoord> adjacentTiles;
+		TileCoord N = tileposition + NORTH;
+		TileCoord S = tileposition + SOUTH;
+		TileCoord E = tileposition + EAST;
+		TileCoord W = tileposition + WEST;
+		if (isTileTraversable(N)) adjacentTiles.push_back(N);
+		if (isTileTraversable(S)) adjacentTiles.push_back(S);
+		if (isTileTraversable(E)) adjacentTiles.push_back(E);
+		if (isTileTraversable(W)) adjacentTiles.push_back(W);
+		return adjacentTiles;
+	}
+	std::vector<Vector2> GetAdjacentTilesV(TileCoord tileposition)
+	{
+		std::vector<Vector2> adjacentTiles;
+		TileCoord N = tileposition + NORTH;
+		TileCoord S = tileposition + SOUTH;
+		TileCoord E = tileposition + EAST;
+		TileCoord W = tileposition + WEST;
+		if (isTileTraversable(N)) adjacentTiles.push_back(GetScreenPosOfTile(tileposition + NORTH));
+		if (isTileTraversable(S)) adjacentTiles.push_back(GetScreenPosOfTile(tileposition + SOUTH));
+		if (isTileTraversable(E)) adjacentTiles.push_back(GetScreenPosOfTile(tileposition + EAST));
+		if (isTileTraversable(W)) adjacentTiles.push_back(GetScreenPosOfTile(tileposition + WEST));
+		return adjacentTiles;
+	}
+
+	bool tileOOB(TileCoord tilepos)
+	{
+		if (tilepos.x < MAP_WIDTH && tilepos.x >= 0 && tilepos.y < MAP_HEIGHT && tilepos.y >= 0)
+		{
+			return false;
+		}
+		else return true;
+	}
+	bool tileOOBV(TileCoord tilepos)
+	{
+		if (tilepos.x < MAP_WIDTH * tileSizeX && tilepos.x >= 0 && tilepos.y < MAP_HEIGHT * tileSizeY && tilepos.y >= 0)
+		{
+			return false;
+		}
+		else return true;
+	}
+	void Draw() 
 	{
 		for (int x = 0; x < MAP_WIDTH; x++)
 		{
 			for (int y = 0; y < MAP_HEIGHT; y++)
 			{
-				Vector2 position = ScreenPosOfTile({ x,y });
+				Color color{ 0,0,0,0 };
 				Tile tile = tiles[x][y];
-				Color color{ 0,0,0,0};
+				Vector2 position = GetScreenPosOfTile({ x,y });
+				
+				color = tileColors[tile];
 				DrawRectangleV(position, { (float)tileSizeX, (float)tileSizeY }, color );
+			}
+		}
+	}
+	void DrawPaths()
+	{
+		for (int x = 0; x < MAP_WIDTH; x++)
+		{
+			for (int y = 0; y < MAP_HEIGHT; y++)
+			{
+				TileCoord tile = { x, y };
+				if (isTileTraversable(tile))
+				{			
+					std::vector<Vector2> adjacent = (GetAdjacentTilesV(tile));
+					Vector2 offSet = { tileSizeX / 2,tileSizeY / 2 };
+					DrawCircleV(GetScreenPosOfTile(tile) + offSet, { tileSizeX / 4 }, LIME);
+
+					for (auto t : adjacent)
+					{
+					 
+						if (!tileOOBV(t)&&(!(t.x < GetScreenPosOfTile(tile).x) || !(t.x < GetScreenPosOfTile(tile).y)))
+						{
+							DrawLineV(t + offSet, GetScreenPosOfTile(tile) + offSet, LIME);
+						}
+ 
+				
+					}
+
+				}
 			}
 		}
 	}
--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,32 +1,107 @@
 #include "rlImGui.h"
 #include "Physics.h"
 #include "Collision.h"
-
+#include "Tilemap.h"
 #include <array>
 #include <vector>
 #include <string>
 #include <iostream>
 #include <fstream>
 
+#include "rlImGui.h"
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+Tilemap map;
 using namespace std;
+
+struct tileAgent
+{
+    TileCoord pos;
+    Color color;
+    float height;
+    tileAgent(Vector2 start, Color c)
+    {
+        pos = start;
+        color = c;
+        height = map.tileSizeX;
+
+    }
+    
+    void Draw()
+    {
+        DrawRectangleV(map.GetScreenPosOfTile(pos), { height, height }, RED);
+    }
+
+};
 
 int main(void)
 {
-   
-            // Render GUI
-        if (IsKeyPressed(KEY_GRAVE)) demoGUI = !demoGUI;
-        if (demoGUI)
+    srand(time(NULL));
+    map.RandomizeTiles();
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sunshine");
+    bool useGUI = false;
+    SetTargetFPS(60);
+    tileAgent player({ 0,0 },RED);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        vector<TileCoord> playerAdjacent = (map.GetAdjacentTiles(player.pos));
+        if (IsKeyPressed(KEY_W) && (map.isTileTraversable({ player.pos.x,player.pos.y - 1 })))
         {
-            rlImGuiBegin();
-            ImGui::ShowDemoWindow(nullptr);
-            rlImGuiEnd();
+            player.pos.y -= 1;
         }
+        else if (IsKeyPressed(KEY_S) && (map.isTileTraversable({ player.pos.x,player.pos.y + 1 })))
+        {
+            player.pos.y +=1;
+        }
+        else if (IsKeyPressed(KEY_A) && (map.isTileTraversable({ player.pos.x-1,player.pos.y })))
+        {
+            player.pos.x -= 1;
+        }
+        else if (IsKeyPressed(KEY_D) && (map.isTileTraversable({ player.pos.x + 1,player.pos.y })))
+        {
+            player.pos.x += 1;
+        }
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+        {
+            TileCoord tile = { GetMousePosition().x / 32,GetMousePosition().y / 32 };
+            player.pos = tile;
+        }
+        map.Draw();
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+        {
+            TileCoord tile = { GetMousePosition().x / 32,GetMousePosition().y / 32 };
+
+            if (tile.x < MAP_WIDTH && tile.y < MAP_HEIGHT)
+            {
+                // std::cout << tile.x << " | " << tile.y << std::endl;
+               //  std::cout << map.isTileTraversable(tile)<<std::endl;
+                //std::cout << map.tiles[tile.x][tile.y] << std::endl;
+                if (map.isTileTraversable(tile))
+                {
+                    vector<TileCoord> adjacent = (map.GetAdjacentTiles(tile));
+                    for (auto t : adjacent)
+                    {
+                        DrawRectangleV(map.GetScreenPosOfTile(t), { map.tileSizeX, map.tileSizeX }, PINK);
+                      //  cout << t.x << " | " << t.y << " [] " << map.GetScreenPosOfTile(tile  ).x << " | " << map.GetScreenPosOfTile(tile  ).y << endl;
+                      //  cout << map.GetScreenPosOfTile(tile + NORTH).x << " | " << map.GetScreenPosOfTile(tile + NORTH).y << endl;
+                 //       std::cout << t.x << "|" << t.y << " [] ";
+                      // cout << map.GetScreenPosOfTile(tile+NORTH).y << " N " << map.GetScreenPosOfTile(tile + SOUTH).y << " S " << map.GetScreenPosOfTile(tile+EAST).x << " E " << map.GetScreenPosOfTile(tile+WEST).x << " W " << endl;
+                    }
+                    // std::cout << std::endl;
+                }
+            }
+            
+        }
+        map.DrawPaths();
+        player.Draw();
 
         EndDrawing();
     }
+   
 
-    rlImGuiShutdown();
     CloseWindow();
-
     return 0;
 }
